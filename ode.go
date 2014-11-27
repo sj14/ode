58573fc36eeae710946d1aab5a83ee7eedaabd9e
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2014-11-27 19:01:03 +0100

    fixed runge-kutta method

--- ode.go
+++ ode.go
@@ -4,8 +4,6 @@ package ode
 
 import "fmt"
 
-// BUG(sj14): The Runge-Kutta Method doesn't seem to work properly (not precise)
-
 func EulerForward(from, h, to, t float64, y []float64, fn func(float64, []float64) []float64) [][]float64 {
 	var steps int = int(to - from/h)
 	var parameters = len(y)
@@ -58,40 +56,69 @@ func RungeKutta(from, h, to, t float64, y []float64, fn func(float64, []float64)
 	var k2 []float64 = make([]float64, parameters)
 	var k3 []float64 = make([]float64, parameters)
 	var k4 []float64 = make([]float64, parameters)
+	
+	var yn []float64 = make([]float64, parameters)
+	
+	// the parameters to pass into the corresponding function calls
+	var k2p []float64 = make([]float64, parameters)
+	var k3p []float64 = make([]float64, parameters)
+	var k4p []float64 = make([]float64, parameters)
+	
+
 
 	for step := 1; step < steps; step++ {
 		// initialize 'inner slice'
 		ySlice[step] = make([]float64, parameters+1)
-
-		// TODO At the begin or end of the loop????
+		
+		// get last yn from the slice to make the rest of the function shorter
 		for value := 0; value < parameters; value++ {
-			y[value] += h * fn(t, y)[value]
+			yn[value] = ySlice[step-1][value+1]
 		}
 
+		// generate k1
 		for value := 0; value < parameters; value++ {
-			k1[value] = h * 0.5 * fn(t, y)[value]
+			k1[value] = fn(t, yn)[value]
+		}
+
+		t += h / 2
+		
+		// generate the parameter for k2
+		for value := 0; value < parameters; value++ {
+			k2p[value] = yn[value] + 0.5 * k1[value]
+		}
+
+		// generate k2
+		for value := 0; value < parameters; value++ {
+			k2[value] = fn(t, k2p)[value]
+		}
+		
+		// generate the parameter for k3
+		for value := 0; value < parameters; value++ {
+			k3p[value] = yn[value] + 0.5 * k2[value]
+		}
+		
+		// generate k3
+		for value := 0; value < parameters; value++ {
+			k3[value] = fn(t, k3p)[value]
+		}
+		
+		// generate the parameter for k4
+		for value := 0; value < parameters; value++ {
+			k4p[value] = yn[value] + fn(t, k3p)[value]
 		}
 
 		t += h / 2
 
+		// generate k4
 		for value := 0; value < parameters; value++ {
-			k2[value] = h * 0.5 * fn(t, k1)[value]
-		}
-
-		for value := 0; value < parameters; value++ {
-			k3[value] = h * fn(t, k2)[value]
-		}
-
-		t += h / 2
-
-		for value := 0; value < parameters; value++ {
-			k4[value] = h * fn(t, k3)[value]
+			k4[value] = fn(t, k4p)[value]
 		}
 
 		ySlice[step][0] = t
 
+		// generate yn (saved in the slice)
 		for value := 0; value < parameters; value++ {
-			ySlice[step][value+1] = y[value] + (k1[value]+2*k2[value]+2*k3[value]+k4[value])/6
+			ySlice[step][value+1] = ySlice[step-1][value+1] + (k1[value]+2*k2[value]+2*k3[value]+k4[value])/6.0
 		}
 	}
 	return ySlice
