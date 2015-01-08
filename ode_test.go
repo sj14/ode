13309d6460335eca82002dbf43fd38e1eff50923
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2015-01-08 10:32:41 +0100

    Population Growth added as Benchmark

--- ode_test.go
+++ ode_test.go
@@ -1,26 +1,43 @@
-// from fib_test.go
 package ode
 
 import "testing"
 import "os"
 
-var yStart []float64 = []float64{}
+var yStartSIR []float64 = []float64{}
+var yStartPop []float64 = []float64{}
 
 func TestMain(m *testing.M) {
 	// SIR Start Values
-	yStart = append(yStart, 700)
-	yStart = append(yStart, 400)
-	yStart = append(yStart, 100)
-	
+	yStartSIR = append(yStartSIR, 700)
+	yStartSIR = append(yStartSIR, 400)
+	yStartSIR = append(yStartSIR, 100)
+
+	// PopulationGrowthSimple Start Values
+	yStartPop = append(yStartPop, 10)
+
 	os.Exit(m.Run())
 }
 
-func BenchmarkEulerForward(b *testing.B) {
-	EulerForward(0, 0.1, 10000, yStart, sir)
+func BenchmarkEulerForwardPopulation(b *testing.B) {
+	EulerForward(0, 0.1, 1000, yStartPop, populationGrowthSimple)
 }
 
-func BenchmarkRungeKutta(b *testing.B) {
-	RungeKutta4(0, 0.1, 10000, yStart, sir)
+func BenchmarkRungeKuttaPopulation(b *testing.B) {
+	RungeKutta4(0, 0.1, 1000, yStartPop, populationGrowthSimple)
+}
+
+func BenchmarkEulerForwardSIR(b *testing.B) {
+	EulerForward(0, 0.1, 10000, yStartSIR, sir)
+}
+
+func BenchmarkRungeKuttaSIR(b *testing.B) {
+	RungeKutta4(0, 0.1, 10000, yStartSIR, sir)
+}
+
+func populationGrowthSimple(t float64, y []float64) []float64 {
+	result := make([]float64, 1)
+	result[0] = 0.008 * y[0]
+	return result
 }
 
 func sir(t float64, y []float64) []float64 {
